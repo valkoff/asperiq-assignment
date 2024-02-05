a40bf51604a01115e8e9c962ad9c577f293ca8b0
Valerio Cofano <cofano@medacta.ch>
2024-02-05 16:58:20 +0100

    feat: provide endpoints to get all, disable and delete routes

---
+++ server/app/Services/RouteService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Services;
+
+use Exception;
+
+readonly class RouteService
+{
+
+    public function __construct(private NetstatService $netstat)
+    {
+    }
+
+    public function getAll(): array
+    {
+        return $this->netstat->getDummyRoutes();
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function delete(string $destination): true
+    {
+        throw_if(!$this->canDeleteRoute($destination), new Exception('Route cannot be deleted'));
+        /**
+         * @todo Implement the actual deletion of the route
+         */
+        return true;
+    }
+
+    /**
+     * @param  string  $destination
+     *
+     * @return bool
+     * Mocks the actual check if the route can be deleted
+     */
+    private function canDeleteRoute(string $destination): bool
+    {
+        return $destination !== '';
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function disable(string $destination): true
+    {
+        throw_if(!$this->canDisableRoute($destination), new Exception('Route cannot be disabled'));
+        /**
+         * @todo Implement the actual disabling of the route
+         */
+        return true;
+    }
+
+    /**
+     * @param  string  $destination
+     *
+     * @return bool
+     * Mocks the actual check if the route can be disabled
+     */
+    private function canDisableRoute(string $destination): bool
+    {
+        return $destination !== '';
+    }
+}
