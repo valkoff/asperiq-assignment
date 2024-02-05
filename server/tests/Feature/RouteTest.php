37a88744b1ef1ba728630dec8a45e70033d6bfcf
Valerio Cofano <cofano@medacta.ch>
2024-02-05 16:19:05 +0100

    feat: add feature tests for route endpoints

---
+++ server/tests/Feature/RouteTest.php
@@ -0,0 +1,27 @@
+<?php
+
+it('should return a list of routes', function () {
+    $response = $this->get('/api/v1/routes');
+    $response->assertStatus(200);
+    $response->assertJson([
+        'data' => [
+            [
+                'destination' => '127.0.0.1',
+                'gateway' => 'link#13',
+                'flags' => 'UH',
+                'netif' => 'lo0',
+                'expire' => null,
+            ],
+        ],
+    ]);
+});
+
+it('should delete a route', function () {
+    $response = $this->delete('/api/v1/routes/1');
+    $response->assertStatus(204);
+});
+
+it('should disable a route', function () {
+    $response = $this->post('/api/v1/routes/2/disable');
+    $response->assertStatus(204);
+});
