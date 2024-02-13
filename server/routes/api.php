01394daf4cf7cdea7eb48d8ef768638c0bf5c7d7
Valerio Cofano <cofano@medacta.ch>
2024-02-13 10:59:02 +0100

    feat: change the api request of disable route into a status change route.

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RouteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::group(['prefix' => 'routes'], function () {
+        Route::get('/', [RouteController::class, 'index']);
+        Route::delete('{destination}', [RouteController::class, 'delete']);
+        Route::post('{destination}', [RouteController::class, 'statusChange']);
+    });
 });
--- server/tests/Feature/RouteTest.php
+++ server/tests/Feature/RouteTest.php
@@ -1,6 +1,6 @@
 <?php
 
-it('should return a list of routes', function () {
+it('can return a list of routes', function () {
     $response = $this->get('/api/v1/routes');
     $response->assertStatus(200);
     $response->assertJson([
@@ -16,12 +16,21 @@ it('should return a list of routes', function () {
     ]);
 });
 
-it('should delete a route', function () {
-    $response = $this->delete('/api/v1/routes/1');
+it('can delete a route', function () {
+    $response = $this->delete('/api/v1/routes/127.0.0.1');
     $response->assertStatus(204);
 });
 
-it('should disable a route', function () {
-    $response = $this->post('/api/v1/routes/2/disable');
+it('can disable a route', function () {
+    $response = $this->post('/api/v1/routes/127.0.0.1', [
+        'enabled' => false,
+    ]);
+    $response->assertStatus(204);
+});
+
+it('can enable a route', function () {
+    $response = $this->post('/api/v1/routes/127.0.0.1', [
+        'enabled' => true,
+    ]);
     $response->assertStatus(204);
 });
